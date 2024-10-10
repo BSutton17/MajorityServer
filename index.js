73c2a5c17cb3a4258e5642cdfbc1ff1d70abbae0
Bryson Sutton <139702261+BSutton17@users.noreply.github.com>
2024-10-10 01:25:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ let rooms = {};
 let playerAnswers = {};
 let playerNames = {};
 let lastPlayerToAnswer = {};
+let lastRandomPlayer = {}; 
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -28,8 +29,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("broadcast_prompt", (msg, room) => {
-    console.log("room: " + room)
-    console.log("msg: " + msg)
     io.to(room).emit("display_prompt", msg);
   });
 
@@ -38,56 +37,50 @@ io.on("connection", (socket) => {
     io.to(room).emit("reset_game");
   });
   socket.on("player_answer", ({ room, name, answer }) => {
-
-    // Initialize room and playerAnswers if not already existing
     if (!rooms[room]) {
       rooms[room] = { players: [], liarSet: false };
     }
-
-    // If it's a new name, add it to the room
+  
     if (!rooms[room].players.includes(name)) {
       rooms[room].players.push(name);
     }
-
+  
     if (!playerAnswers[room]) {
       playerAnswers[room] = {};
     }
-
-    // Store the player's answer
+  
     playerAnswers[room][name] = answer;
-
+  
     // Track the last player to answer
     lastPlayerToAnswer[room] = name;
-
+  
     const playersInRoom = rooms[room].players.length;
     const answersInRoom = Object.keys(playerAnswers[room]).length;
-
-    
+  
     // Check if all players have answered
     if (playersInRoom === answersInRoom) {
-      const changeAnswerIndex = Math.floor(Math.random() * playersInRoom);
-      const randomPlayer = rooms[room].players[changeAnswerIndex];
-
-    // Change the selected player's answer if they have answered already
-    if (playerAnswers[room][randomPlayer]) {
-      playerAnswers[room][randomPlayer] = playerAnswers[room][randomPlayer] === "Yes" ? "No" : "Yes";
-
-      // Notify the specific player whose answer was changed
-      const socketId = Object.keys(playerNames).find(id => playerNames[id] === randomPlayer);
-      console.log(socket.id);
-      
-      if (socketId) {
-        io.to(socketId).emit("answer_changed", "Your answer has been changed. Lie your way through.");
-        console.log("should be working")
+      const availablePlayers = rooms[room].players;
+  
+      // Select a random player (without excluding the last random player)
+      const changeAnswerIndex = Math.floor(Math.random() * availablePlayers.length);
+      const randomPlayer = availablePlayers[changeAnswerIndex];
+  
+      lastRandomPlayer[room] = randomPlayer;
+  
+      if (playerAnswers[room][randomPlayer]) {
+        playerAnswers[room][randomPlayer] = playerAnswers[room][randomPlayer] === "Yes" ? "No" : "Yes";
+  
+        const socketId = Object.keys(playerNames).find(id => playerNames[id] === randomPlayer);
+        if (socketId) {
+          io.to(socketId).emit("answer_changed", "Your answer has been changed. Lie your way through.");
+        }
       }
-      console.log(playerNames);
-    }
-
+  
       io.to(room).emit("show_results", true);
     }
-    
     io.to(room).emit("receive_answers", playerAnswers[room]);
   });
+  
 
   socket.on("join_room", (room, name ) => {
     socket.join(room);
