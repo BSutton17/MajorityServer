13ad5a42393d1665097ed95bfcd7dcf46371ecfa
Bryson Sutton <139702261+BSutton17@users.noreply.github.com>
2025-01-12 22:26:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://jeopardy-buzzer.netlify.app",
+    origin: "https://hypothetical.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
